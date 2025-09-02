dddb01f24418235516f00d2db025da749552f92b
donjd <99505859+donjd@users.noreply.github.com>
2025-09-01 22:56:59 -0600

    Add eyeball/ password showing functionality

---
+++ main.js
@@ -0,0 +1,21 @@
+const eyeIcons = document.querySelectorAll(".fa-eye");
+const passwordInputs = document.querySelectorAll('input[type="password"]');
+
+function toggleEyeAccent(eyeIcon) {
+  eyeIcon.classList.toggle("accent-1");
+}
+
+function togglePasswordVisibility(passwordInput) {
+  if (passwordInput.type === "password") {
+    passwordInput.setAttribute("type", "text");
+  } else {
+    passwordInput.setAttribute("type", "password");
+  }
+}
+
+eyeIcons.forEach((eyeIcon, i) => {
+  eyeIcon.addEventListener("click", () => {
+    toggleEyeAccent(eyeIcon);
+    togglePasswordVisibility(passwordInputs[i]);
+  });
+});
