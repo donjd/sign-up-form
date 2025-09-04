a73861a84b444ae71f9e344330ee738eb715f6d5
donjd <99505859+donjd@users.noreply.github.com>
2025-09-03 23:08:20 -0600

    Add invalid styling

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const eyeIcons = document.querySelectorAll(".fa-eye");
 const passwordInputs = document.querySelectorAll('input[type="password"]');
+const inputs = document.querySelectorAll("input");
+const password = document.querySelector(".password");
+const passwordConfirmation = document.querySelector(".password-confirmation");
+const submitBtn = document.querySelector('button[type="submit"]');
 
 function toggleEyeAccent(eyeIcon) {
   eyeIcon.classList.toggle("accent-1");
@@ -24,4 +28,27 @@ eyeIcons.forEach((eyeIcon) => {
   eyeIcon.addEventListener("click", (e) => {
     console.log(e);
   });
+});
+
+function addInvalidClass(element) {
+  element.classList.add("invalid");
+}
+function removeInvalidClass(element) {
+  element.classList.remove("invalid");
+}
+
+submitBtn.addEventListener("click", (e) => {
+  if (password.value === "") {
+    addInvalidClass(password);
+    e.preventDefault();
+  } else {
+    removeInvalidClass(password);
+  }
+
+  if (passwordConfirmation.value === "") {
+    addInvalidClass(passwordConfirmation);
+    e.preventDefault();
+  } else {
+    removeInvalidClass(passwordConfirmation);
+  }
 });
