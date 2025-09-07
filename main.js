0a4efa14d7e43121ef64e37d2b5dc537536d6b90
donjd <99505859+donjd@users.noreply.github.com>
2025-09-06 22:28:50 -0600

    Clean up code and add error messages to array

--- main.js
+++ main.js
@@ -1,54 +1,84 @@
-const eyeIcons = document.querySelectorAll(".fa-eye");
-const passwordInputs = document.querySelectorAll('input[type="password"]');
-const inputs = document.querySelectorAll("input");
-const password = document.querySelector(".password");
-const passwordConfirmation = document.querySelector(".password-confirmation");
+const firstName = document.querySelector("#first-name");
+const lastName = document.querySelector("#last-name");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const passwordConfirmation = document.querySelector("#password-confirmation");
 const submitBtn = document.querySelector('button[type="submit"]');
+const form = document.querySelector("form");
 
-function toggleEyeAccent(eyeIcon) {
-  eyeIcon.classList.toggle("accent-1");
-}
-
-function togglePasswordVisibility(passwordInput) {
-  if (passwordInput.type === "password") {
-    passwordInput.setAttribute("type", "text");
+function togglePasswordVisibility(passwordType) {
+  if (passwordType.type == "password") {
+    passwordType.setAttribute("type", "text");
   } else {
-    passwordInput.setAttribute("type", "password");
+    passwordType.setAttribute("type", "password");
   }
 }
 
-eyeIcons.forEach((eyeIcon, i) => {
-  eyeIcon.addEventListener("click", () => {
-    toggleEyeAccent(eyeIcon);
-    togglePasswordVisibility(passwordInputs[i]);
-  });
-});
+function toggleEyeColor(e) {
+  switch (true) {
+    case e.target.classList.contains("password-visibility-toggle"):
+      togglePasswordVisibility(password);
+      break;
 
-eyeIcons.forEach((eyeIcon) => {
-  eyeIcon.addEventListener("click", (e) => {
-    console.log(e);
-  });
-});
-
-function addInvalidClass(element) {
-  element.classList.add("invalid");
-}
-function removeInvalidClass(element) {
-  element.classList.remove("invalid");
-}
-
-submitBtn.addEventListener("click", (e) => {
-  if (password.value === "") {
-    addInvalidClass(password);
-    e.preventDefault();
-  } else {
-    removeInvalidClass(password);
+    case e.target.classList.contains("password-confirmation-visibility-toggle"):
+      togglePasswordVisibility(passwordConfirmation);
+      break;
   }
+  e.target.classList.toggle("accent-1");
+}
 
-  if (passwordConfirmation.value === "") {
-    addInvalidClass(passwordConfirmation);
-    e.preventDefault();
-  } else {
-    removeInvalidClass(passwordConfirmation);
+function addInvalidClass(e) {
+  e.classList.add("invalid");
+}
+
+function removeInvalidClass(e) {
+  e.classList.remove("invalid");
+}
+
+form.addEventListener("click", (e) => {
+  let errors = [];
+  switch (true) {
+    case e.target.classList.contains("fa-eye"):
+      toggleEyeColor(e);
+      break;
+
+    case e.target.type === "submit":
+      if (firstName.value === "" || null) {
+        errors.push("Please enter your first name.");
+        addInvalidClass(firstName);
+      } else {
+        removeInvalidClass(firstName);
+      }
+
+      if (lastName.value === "" || null) {
+        errors.push("Please enter your last name.");
+        addInvalidClass(lastName);
+      } else {
+        removeInvalidClass(lastName);
+      }
+      if (email.value === "" || null) {
+        errors.push("Please enter your last name.");
+        addInvalidClass(email);
+      } else {
+        removeInvalidClass(email);
+      }
+      if (password.value === "" || null) {
+        errors.push("Please enter a password.");
+        addInvalidClass(password);
+      } else {
+        removeInvalidClass(password);
+      }
+
+      if (passwordConfirmation.value === "" || null) {
+        errors.push("Please confirm password.");
+        addInvalidClass(passwordConfirmation);
+      } else {
+        removeInvalidClass(passwordConfirmation);
+      }
+
+      if (errors.length > 0) {
+        e.preventDefault();
+      }
+      break;
   }
 });
