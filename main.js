fb2e843269123093b25451b634270b1b4f6f6d2f
donjd <99505859+donjd@users.noreply.github.com>
2025-09-10 23:25:50 -0600

    log in doesn't work

--- main.js
+++ main.js
@@ -1,58 +1,18 @@
+const form = document.querySelector("form");
+const submitBtn = document.querySelector('button[type="submit"]');
+
 const firstName = document.querySelector("#first-name");
 const lastName = document.querySelector("#last-name");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const passwordConfirmation = document.querySelector("#password-confirmation");
 
-const submitBtn = document.querySelector('button[type="submit"]');
-const form = document.querySelector("form");
-
-// function validateLogInForm() {
-//   form.addEventListener("click", (e) => {
-//     let errors = [];
-//     switch (true) {
-//       case e.target.classList.contains("fa-eye"):
-//         toggleEyeColor(e);
-//         break;
-
-//       case e.target.type === "submit":
-//         if (email.value === "" || null) {
-//           errors.push("Please enter your email.");
-//           addInvalidClass(email);
-//         } else {
-//           removeInvalidClass(email);
-//         }
-
-//         if (password.value === "" || null) {
-//           errors.push("Please enter your password.");
-//           addInvalidClass(password);
-//         } else {
-//           removeInvalidClass(password);
-//         }
-
-//         if (errors.length > 0) {
-//           console.log(errors);
-//           e.preventDefault();
-//         }
-//         break;
-//     }
-//   });
-
-//   form.addEventListener("input", (e) => {
-//     if (e.target.classList.contains("invalid")) {
-//       e.target.classList.remove("invalid");
-//     }
-//   });
-// }
-
 const passwordVisibilityToggle = document.querySelector(
   ".password-visibility-toggle"
 );
-
 const passwordConfirmationVisibilityToggle = document.querySelector(
   ".password-confirmation-visibility-toggle"
 );
-
 const passwordEye = document.querySelector(".password-eye");
 const passwordConfirmationEye = document.querySelector(
   ".password-confirmation-eye"
@@ -77,60 +37,68 @@ togglePasswordVisibility(
   passwordConfirmationEye
 );
 
+let errors = [];
+
+function validateInput(inputType) {
+  if (!inputType.value) {
+    errors.push("Please fill out all fields.");
+    inputType.classList.add("invalid");
+  }
+}
+
+function validatePasswordsMatch() {
+  if (password.value !== passwordConfirmation.value) {
+    errors.push("Passwords must match.");
+    password.classList.add("invalid");
+    passwordConfirmation.classList.add("invalid");
+  } else {
+    password.classList.remove("invalid");
+    passwordConfirmation.classList.remove("invalid");
+  }
+}
+
 function validateSignUpForm() {
   form.addEventListener("click", (e) => {
-    let errors = [];
+    validatePasswordsMatch();
+    validateInput(firstName);
+    validateInput(lastName);
+    validateInput(email);
+    validateInput(password);
+    validateInput(passwordConfirmation);
 
-    switch (true) {
-      case e.target.classList.contains("fa-eye"):
-        e.target.classList.toggle("invalid");
-        break;
-
-      case e.target.type === "submit":
-        if (firstName.value === "" || null) {
-          errors.push("Please enter your first name.");
-          addInvalidClass(firstName);
-        } else {
-          removeInvalidClass(firstName);
-        }
-
-        if (lastName.value === "" || null) {
-          errors.push("Please enter your last name.");
-          addInvalidClass(lastName);
-        } else {
-          removeInvalidClass(lastName);
-        }
-
-        if (email.value === "" || null) {
-          errors.push("Please enter your email.");
-          addInvalidClass(email);
-        } else {
-          removeInvalidClass(email);
-        }
-
-        if (password.value === "" || null) {
-          errors.push("Please enter a password.");
-          addInvalidClass(password);
-        } else {
-          removeInvalidClass(password);
-        }
-
-        if (passwordConfirmation.value === "" || null) {
-          errors.push("Please confirm password.");
-          addInvalidClass(passwordConfirmation);
-        } else if (password.value !== passwordConfirmation.value) {
-          errors.push("Password confirmation must match password.");
-          addInvalidClass(passwordConfirmation);
-        } else {
-          removeInvalidClass(passwordConfirmation);
-        }
-
-        if (errors.length > 0) {
-          console.log(errors);
-          e.preventDefault();
-        }
-        break;
+    if (errors.length > 0) {
+      console.log(errors);
+      e.preventDefault();
+      errors = [];
     }
+  });
+
+  form.addEventListener("input", (e) => {
+    if (e.target.classList.contains("invalid")) {
+      if (
+        e.target.type === "password" &&
+        e.target.classList.contains("invalid")
+      ) {
+        validatePasswordsMatch();
+      } else {
+        e.target.classList.remove("invalid");
+        errors = [];
+      }
+    }
+  });
+}
+
+function validateLogInForm() {
+  form.addEventListener("click", (e) => {
+    validateInput(email);
+    validateInput(password);
+
+    if (errors.length > 0) {
+      console.log(errors);
+      e.preventDefault();
+    }
+
+    errors = [];
   });
 
   form.addEventListener("input", (e) => {
